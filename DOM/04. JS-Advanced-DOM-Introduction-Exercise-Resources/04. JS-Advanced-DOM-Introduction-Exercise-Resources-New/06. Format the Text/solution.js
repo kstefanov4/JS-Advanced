44afb87673d233a6f1bd0fa2dee6a2f409d7162c
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-09-23 21:38:48 +0300

    JS

--- DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-  //TODO
+  let text = document.getElementById('input').value;
+  let sentensesArray = text.split('.');
+  sentensesArray.pop();
+  let output = document.getElementById('output');
+  output.innerHTML = '';
+
+  for (let i = 0; i < sentensesArray.length; i += 3){
+    let resultArray = [];
+
+    for (let j = 0; j < 3; j++){
+      if (sentensesArray[i + j]){
+        resultArray.push(sentensesArray[i + j]);
+      }
+    }
+    
+    let text = resultArray.join('. ') + '.'.trim();
+    output.innerHTML += `<p>${text}</p>`;
+  }
 }
