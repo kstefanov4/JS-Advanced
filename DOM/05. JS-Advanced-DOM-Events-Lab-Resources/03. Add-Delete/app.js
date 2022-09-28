386d54464f8c9808deffa55773d95d8a80eff822
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-09-28 11:30:28 +0300

    js

--- DOM/05. JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
+++ DOM/05. JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
@@ -1,3 +1,13 @@
 function addItem() {
-    //TODO...
+    let input = document.getElementById('newItemText').value;
+
+    let liElement = document.createElement('li');
+    let aElement = document.createElement('a');
+    aElement.textContent = '[Delete]';
+    aElement.href = '#';
+    aElement.onclick = function(){aElement.parentElement.remove()};
+    liElement.textContent = input;
+    liElement.appendChild(aElement);
+    document.getElementById('items').appendChild(liElement);
+    document.getElementById('newItemText').value = '';
 }
