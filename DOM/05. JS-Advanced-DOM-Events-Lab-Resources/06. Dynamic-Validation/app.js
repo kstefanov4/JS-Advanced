7831a1e648440474cc667d453bfd85f8230a05f1
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-09-28 14:24:22 +0300

    js

--- DOM/05. JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
+++ DOM/05. JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
@@ -1,3 +1,22 @@
 function validate() {
-    console.log('TODO:...');
+    document.getElementById('email').addEventListener('change',change);
+    
+    function change(){
+        let input =  document.getElementById('email').value;
+        console.log(validateEmail(input))
+
+        function validateEmail(input){
+            return String(input)
+              .toLowerCase()
+              .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+              );
+        }
+
+        if (validateEmail(input) == null){
+            document.getElementById('email').className = 'error';
+        }else{
+            document.getElementById('email').className = '';
+        }
+    }
 }
