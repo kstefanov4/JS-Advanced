67c1d94792ae4a4cf338df167e1442868b8d1773
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-09-28 15:40:00 +0300

    JS

--- DOM/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
+++ DOM/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
@@ -1,3 +1,43 @@
 function solve() {
-   //TODO...
+   let elements =Array.from(document.getElementsByClassName('add-product'));
+   console.log(elements)
+
+   for (const element of elements) {
+      element.addEventListener('click', add);
+   }
+
+   let cart = [];
+
+   function add(event) {
+      let childrenList = event.target.parentElement.parentElement.children;
+
+      let price = childrenList[3].childNodes[0].textContent;
+      let productName = childrenList[1].childNodes[1].textContent;
+      let object = { price, productName };
+
+      cart.push(object);
+
+      let currentAdded = document.getElementsByTagName('textarea')[0].value;
+      currentAdded += `Added ${productName} for ${price} to the cart.\n`
+      document.getElementsByTagName('textarea')[0].value = currentAdded;
+
+   }
+
+   document.getElementsByClassName('checkout')[0].addEventListener('click', checkout)
+
+   function checkout() {
+      let totalPrice = 0;
+      let addedProducts = [];
+      for (const product of cart) {
+         totalPrice +=Number(product.price);
+         if (addedProducts.indexOf(product.productName) < 0){
+            addedProducts.push(product.productName);
+         }
+      }
+      
+      let currentAdded = document.getElementsByTagName('textarea')[0].value;
+      currentAdded += `You bought ${addedProducts.join(', ')} for ${totalPrice.toFixed(2)}.`
+      document.getElementsByTagName('textarea')[0].value = currentAdded;
+   }
+
 }
