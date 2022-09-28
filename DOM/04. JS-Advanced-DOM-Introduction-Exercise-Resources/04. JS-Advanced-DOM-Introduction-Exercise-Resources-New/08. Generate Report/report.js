198275c695dc7172cc69120f7964d6dccdc7211a
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-09-28 08:11:11 +0300

    js

--- DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -1,3 +1,29 @@
 function generateReport() {
-    //TODO
+    let tHeadElements = Array.from(document.querySelectorAll('th input'));
+
+    let tableInfo = Array.from(document.querySelectorAll('tbody tr'));
+
+    let indexes = [];
+    let result = [];
+
+    for (let element of tHeadElements) {
+        if (element.checked) {
+            let index = tHeadElements.indexOf(element);
+            indexes.push(index);
+        }
+    }
+
+    let columnNames = Array.from(document.querySelectorAll('thead tr th'));
+
+    for (const row of tableInfo) {
+        let object = {};
+
+        for (const index of indexes) {
+
+            object[columnNames[index].textContent.toLowerCase().trim()] = row.children[index].textContent
+
+        }
+        result.push(object);
+    }
+    document.getElementById('output').value = JSON.stringify(result);
 }
