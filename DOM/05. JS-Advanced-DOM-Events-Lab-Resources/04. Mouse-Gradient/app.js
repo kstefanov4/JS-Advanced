408ac12a63fa2001dcde930f99b1e9f40e322115
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-09-28 11:44:37 +0300

    js

--- DOM/05. JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
+++ DOM/05. JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
@@ -1,3 +1,8 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    document.getElementById('gradient').addEventListener('mousemove',hovered);
+
+    function hovered(event){
+        let percent =Math.trunc(event.offsetX / (event.target.clientWidth - 1) * 100);
+        document.getElementById('result').textContent = percent + '%';
+    }
 }
