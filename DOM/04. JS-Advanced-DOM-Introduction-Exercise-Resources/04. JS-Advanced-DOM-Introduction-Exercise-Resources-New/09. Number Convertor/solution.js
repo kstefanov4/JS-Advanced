8f7e42d12d47bbbd959b154365576995eebe7358
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-09-28 09:59:15 +0300

    JS

--- DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
+++ DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
@@ -1,4 +1,46 @@
 function solve() {
+    // let metaElement = document.createElement('meta');
+    // metaElement.setAttribute('charset', 'UTF-8');
+    // document.getElementsByTagName('head')[0].appendChild(metaElement);
 
-    //TODO...
+    document.querySelector('button').addEventListener('click', onClick);
+
+    
+    let attributes = ['Binary', 'Hexadecimal'];
+    let menuTo = document.getElementById('selectMenuTo');
+
+    for (let i = 0; i < attributes.length; i++) {
+        let optionValue = document.createElement('option');
+        optionValue.setAttribute('selected', '')
+        optionValue.setAttribute('value', attributes[i])
+        optionValue.textContent = attributes[i];
+        menuTo.appendChild(optionValue);
+    }
+
+    document.querySelectorAll('#selectMenuTo option')[0].remove();
+
+
+    function onClick() {
+        let input = document.getElementById('input').value;
+        let sel = document.getElementById("selectMenuTo");
+        let text = sel.options[sel.selectedIndex].text;
+        
+        if (text == 'Binary') {
+            document.getElementById('result').value = '';
+            document.getElementById('result').value = (input >>> 0).toString(2);
+
+        } else if (text == 'Hexadecimal') {
+            document.getElementById('result').value = '';
+            document.getElementById('result').value = Math.abs(input).toString(16).toUpperCase();
+        }
+
+        function binary(input) {
+            return (input >>> 0).toString(2);
+        }
+
+        function hexadecimal(input) {
+            return input.toString(16);
+        }
+    }
+
 }
