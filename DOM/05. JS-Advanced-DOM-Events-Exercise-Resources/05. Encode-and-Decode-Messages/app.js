d65f6ff486146834d6c5e4978fcae09ff6d9e33e
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-09-29 13:57:54 +0300

    js

--- DOM/05. JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
+++ DOM/05. JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,46 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    Array.from(document.getElementsByTagName('button'))
+        .forEach(element => element.addEventListener('click', action));
+
+    let inputText = '';
+    let encodeMessage = '';
+
+
+    function action(event) {
+        let element = event.target.parentElement;
+
+        inputText = element.getElementsByTagName('textarea')[0].value;
+
+        if (element.getElementsByTagName('p')[0].textContent == 'Message') {
+
+            if (document.getElementById('main').children[1]
+                .getElementsByTagName('textarea')[0].value != '') {
+                encodeMessage = '';
+            }
+            for (const letter of inputText) {
+
+                let encodedLetter = letter.charCodeAt() + 1;
+                encodeMessage += String.fromCharCode(encodedLetter);
+            }
+
+            document.getElementById('main').children[1]
+                .getElementsByTagName('textarea')[0].value = encodeMessage;
+
+            element.getElementsByTagName('textarea')[0].value = '';
+
+        } else if (element.getElementsByTagName('p')[0].textContent == 'Last received message') {
+            inputText = encodeMessage;
+            encodeMessage = '';
+            // document.getElementById('main').children[1]
+            //     .getElementsByTagName('textarea')[0].value = '';
+            for (const letter of inputText) {
+
+                let encodedLetter = letter.charCodeAt() - 1;
+                encodeMessage += String.fromCharCode(encodedLetter);
+            }
+            document.getElementById('main').children[1]
+                .getElementsByTagName('textarea')[0].value = encodeMessage;
+        }
+
+    }
 }
