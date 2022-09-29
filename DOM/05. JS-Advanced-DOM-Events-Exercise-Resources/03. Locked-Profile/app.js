ba8d8988ebe15d918145d2685411dec172822c9a
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-09-29 10:07:43 +0300

    js

--- DOM/05. JS-Advanced-DOM-Events-Exercise-Resources/03. Locked-Profile/app.js
+++ DOM/05. JS-Advanced-DOM-Events-Exercise-Resources/03. Locked-Profile/app.js
@@ -1,3 +1,26 @@
 function lockedProfile() {
-    console.log('TODO...')
+   let buttons =Array.from(document.getElementsByTagName('button'));
+
+    for (const button of buttons) {
+        button.addEventListener('click', action);
+
+    }
+
+    function action(event){
+        
+        if (event.target.parentElement.children[4].checked == true){
+            if(event.target.parentElement.children[10].textContent == 'Hide it'){
+                event.target.parentElement.children[9].style.display = 'none';
+                event.target.parentElement.children[10].textContent = 'Show more'
+                return;
+            }
+
+            console.log(event.target.parentElement)
+            event.target.parentElement.children[9].style.display = 'inline-block';
+            
+            if (event.target.parentElement.children[9].style.display == 'inline-block'){
+                event.target.parentElement.children[10].textContent = 'Hide it'
+            }
+        }
+    }
 }
