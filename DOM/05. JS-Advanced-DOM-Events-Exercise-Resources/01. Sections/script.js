0be406a0366b0b54c6564b9fddf0fba631ad5af2
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-09-29 08:13:26 +0300

    js

--- DOM/05. JS-Advanced-DOM-Events-Exercise-Resources/01. Sections/script.js
+++ DOM/05. JS-Advanced-DOM-Events-Exercise-Resources/01. Sections/script.js
@@ -1,3 +1,23 @@
 function create(words) {
-   console.log('TODO:...');
+   let section = document.getElementById('content');
+   //console.log(section)
+
+   for (const paragraph of words) {
+
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.textContent = paragraph;
+      p.style.display = 'none';
+      div.appendChild(p);
+      section.appendChild(div);
+   }
+   let sectionChildren = Array.from(section.children);
+   console.log(sectionChildren)
+
+   for (const child of sectionChildren) {
+      child.addEventListener('click', showText);
+   }
+   function showText(event) {
+      console.log(event.target.children[0].style.display = '')
+   }
 }
