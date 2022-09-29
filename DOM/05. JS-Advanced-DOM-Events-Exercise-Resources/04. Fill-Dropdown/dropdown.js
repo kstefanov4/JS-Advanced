2bde0fa56c4ec75a04d6c3ef1265168fad66eacb
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-09-29 10:43:15 +0300

    js

--- DOM/05. JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
+++ DOM/05. JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    let text = document.getElementById('newItemText').value;
+    let value = document.getElementById('newItemValue').value;
+
+    let optionElement = document.createElement('option');
+    optionElement.textContent = text;
+    optionElement.value = value;
+    document.getElementById('menu').appendChild(optionElement);
+
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = '';
 }
