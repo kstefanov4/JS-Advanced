f9af32d867444cc9848b250af14788bac96fb540
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-10-04 17:08:36 +0300

    js

--- AdvancedFunctions/07. Simple Calculator/app.js
+++ AdvancedFunctions/07. Simple Calculator/app.js
@@ -1,7 +1,26 @@
 function calculator() {
-    // TODO:
+    let numOne;
+    let numTwo;
+    let result;
+
+    return {
+        init: (selector1, selector2, resultSelector) => {
+            numOne = document.querySelector(selector1);
+            numTwo = document.querySelector(selector2);
+            result = document.querySelector(resultSelector);
+        },
+        add: () => {
+            let firstNum = Number(numOne.value);
+            let secondNum = Number(numTwo.value);
+            result.value = firstNum + secondNum;
+        },
+        subtract: () => {
+            let firstNum = Number(numOne.value);
+            let secondNum = Number(numTwo.value);
+            result.value = firstNum - secondNum;
+        }
+    }
 }
 
-
-
-
+const calculate = calculator (); 
+calculate.init ('#num1', '#num2', '#result'); 
