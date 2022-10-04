68031355317c8aa232062a094d18e86534f128a0
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-10-04 17:25:14 +0300

    js

--- AdvancedFunctions/08. Next Article/script.js
+++ AdvancedFunctions/08. Next Article/script.js
@@ -1,3 +1,12 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let currentArticles = Array.from(articles);
+
+    return () => {
+        if (currentArticles.length === 0) {
+            return
+        }
+
+        let article = currentArticles.shift();
+        document.getElementById('content').innerHTML += `<article>${article}</article>`
+    }
 }
